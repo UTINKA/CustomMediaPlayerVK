0d71e0e704ab8dcb097eb0538cfd7b55f6778063
UTINKA <artem900079@gmail.com>
2019-04-23 23:15:55 +0300

    Update

--- CustomMediaPlayerVK.user.js
+++ CustomMediaPlayerVK.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
-// @date			27.12.2018
+// @date			23.04.2019
 // @name			CustomMediaPlayerVK
 // @namespace		https://github.com/UTINKA/CustomMediaPlayerVK/
-// @version			0.2
+// @version			0.4
 // @description		Изменённый плеер в верхнем меню для ВК
 // @author			UTINKA
 // @include			https://vk.com/*
@@ -21,9 +21,217 @@
 
 (function()
 {
-	var CMP = [];
+	var 
+		CMP = [],
+		SettingsAdd = false,
+		cmpvk = '0,0';
+	;
 	var main_box = $('#top_audio_player');
 	
+
+	function getMem(key)
+	{
+		var memory = localStorage.getItem(key);
+		if(memory != null && memory != 'null' && memory.length != 0) return memory;
+		else return false;
+	}
+	function setMem(key, value)
+	{
+		return localStorage.setItem(key, value);
+	}
+	
+	function UpdateSettings()
+	{
+		var settings = cmpvk[0] + ',' + cmpvk[1];
+		setMem('cmpvk', settings);
+	}
+
+	function Start()
+	{
+		new MutationObserver(function () 
+		{
+			CMPUpdate();
+			if(getMem('cmpvk') != '')
+			{
+				cmpvk = getMem('cmpvk');
+				cmpvk = cmpvk.split(',');
+			}
+			else if(getMem('cmpvk') == false)
+			{
+				var settings_start = new String(cmpvk);
+				cmpvk = settings_start.split(',');
+			}
+			//
+			if(cmpvk[0] == 0)
+			{
+				CMPVKsetState(false);
+				$('#settings_cmpvk_state').prop("checked", false);
+			}
+			else if(cmpvk[0] == 1)
+			{
+				CMPVKsetState(true);
+				$('#settings_cmpvk_state').prop("checked", true);
+			}
+			if(cmpvk[1] == 0)
+			{
+				$('.top_audio_player').css('display','block');
+				$('#settings_cmpvk_player').prop("checked", false);
+			}
+			else if(cmpvk[1] == 1)
+			{
+				$('.top_audio_player').css('display','none');
+				$('#settings_cmpvk_player').prop("checked", true);
+			}
+			//
+			var sbox = $('.settings_panel');
+			if(location.href.search(/settings/) > 1 && location.href.search(/act/) == -1)
+			{
+				if(SettingsAdd == false)
+				{
+					SettingsAdd = true;
+					var 
+						CheckBox, 
+						CheckBoxPlayerState
+					;
+					if(cmpvk[0] == 0) CheckBox = '<input class="blind_label" type="checkbox" id="settings_cmpvk_state">';
+					else if(cmpvk[0] == 1) CheckBox = '<input checked class="blind_label" type="checkbox" id="settings_cmpvk_state">';
+					
+					if(cmpvk[1] == 0) CheckBoxPlayerState = '<input class="blind_label" type="checkbox" id="settings_cmpvk_player">';
+					else if(cmpvk[1] == 1) CheckBoxPlayerState = '<input checked class="blind_label" type="checkbox" id="settings_cmpvk_player">';
+
+					sbox.prepend('\
+						<div class="settings_line">\
+							<div class="settings_info_block">\
+								<div class="settings_label">Custom Media Player</br>Настройки</div>\
+								<div class="settings_labeled_text">\
+									<!--div class="settings_menu_link"></div-->\
+									<div class="settings_narrow_row">\
+										' + CheckBox + '\
+										<label for="settings_cmpvk_state">Использовать плеер</label>\
+									</div>\
+									<div class="settings_narrow_row">\
+										' + CheckBoxPlayerState + '\
+										<label for="settings_cmpvk_player">Скрыть плеер</label>\
+									</div>\
+								</div>\
+							</div>\
+						</div>\
+					');
+					$('#settings_cmpvk_state').change(function(e)
+					{
+						var state = $(this).prop("checked");
+						if(state == true)
+						{
+							cmpvk[0] = 1;
+							UpdateSettings();
+							CMPVKsetState(true);
+						}
+						else 
+						{
+
+							cmpvk[0] = 0;
+							UpdateSettings();
+							CMPVKsetState(false);
+						}
+					});
+					$('#settings_cmpvk_player').change(function(e)
+					{
+						var state = $(this).prop("checked");
+						if(state == true)
+						{
+							cmpvk[1] = 1;
+							UpdateSettings();
+							$('.top_audio_player').css('display','none');
+						}
+						else 
+						{
+							cmpvk[1] = 0;
+							UpdateSettings();
+							$('.top_audio_player').css('display','block');
+						}
+					});
+				}
+			}
+			else
+			{
+				SettingsAdd = false;
+			}
+		}).observe(document.body, {childList: true, subtree: true});
+	}
+	
+	function CMPVKsetState(state)
+	{
+		if(state == false)
+		{
+			main_box.find('.top_audio_player_img').css('display','none');
+			// prev
+			main_box.find('.top_audio_player_prev').css({
+				'position': 'relative',
+				'top': 'unset',
+				'bottom': 'unset',
+				'left': 'unset'
+			});
+			// play/pause
+			main_box.find('.top_audio_player_play').css({
+				'position': 'relative',
+				'top': 'unset',
+				'bottom': 'unset',
+				'left': 'unset'
+			});
+			// next
+			main_box.find('.top_audio_player_next').css({
+				'position': 'relative',
+				'top': 'unset',
+				'bottom': 'unset',
+				'left': 'unset'
+			});
+			//
+			main_box.find('.top_audio_player_title_wrap').css({
+				'position': 'relative',
+				'top': 'unset',
+				'left': 'unset',
+				'right': 'unset',
+				'bottom': 'unset',
+				'margin': '14px 0 0'
+			});
+			main_box.find('.top_audio_player_title').css('display','inline-block');
+		}
+		else
+		{
+			main_box.find('.top_audio_player_img').css('display','block');
+			// prev
+			main_box.find('.top_audio_player_prev').css({
+				'position': 'absolute',
+				'top': '0px',
+				'bottom': '0px',
+				'left': '42px'
+			});
+			// play/pause
+			main_box.find('.top_audio_player_play').css({
+				'position': 'absolute',
+				'top': '0px',
+				'bottom': '0px',
+				'left': '77px'
+			});
+			// next
+			main_box.find('.top_audio_player_next').css({
+				'position': 'absolute',
+				'top': '0px',
+				'bottom': '0px',
+				'left': '107px'
+			});
+			//
+			main_box.find('.top_audio_player_title_wrap').css({
+				'position': 'absolute',
+				'top': '-40px',
+				'left': '137px',
+				'right': '0px',
+				'bottom': '0px',
+				'margin': '0px'
+			});
+			main_box.find('.top_audio_player_title').css('display','none');
+		}
+	}
 
 	// css
 	var CMP_CSS = '\
@@ -104,37 +312,6 @@
 	{
 		if(location.href.search(/widget/)==-1)
 		{
-			main_box.find('.top_audio_player_title').css('display','none');
-			//
-			$('.top_audio_player .top_audio_player_btn').css('position','absolute'); // all buttons
-			// prev 
-			main_box.find('.top_audio_player_btn.top_audio_player_prev').css({
-				'top': '0',
-				'bottom': '0',
-				'left': '42px'
-			});
-			// play/pause
-			main_box.find('.top_audio_player_btn.top_audio_player_play._top_audio_player_play').css({
-				'top': '0',
-				'bottom': '0',
-				'left': '77px'
-			});
-			// next
-			main_box.find('.top_audio_player_btn.top_audio_player_next').css({
-				'top': '0',
-				'bottom': '0',
-				'left': '101px'
-			});
-			//
-			main_box.find('.top_audio_player_title_wrap').css({
-				'position': 'absolute',
-				'top': '-40px',
-				'left': '130px',
-				'right': '0',
-				'bottom': '0'
-			})
-			main_box.find('.top_audio_player_title_wrap').css('margin','0');
-			//
 			main_box.find('.top_audio_player_title_wrap').append('\
 			<div class="top_audio_player_cmp_author"></div>\
 			<div class="top_audio_player_cmp_name"></div>\
@@ -156,9 +333,9 @@
 				e.preventDefault();
 				download_file(main_box.find('.top_audio_player_img').find('div a')[0].href, CMP[0][4] + ' - ' + CMP[0][3], "audio/mp3", window.download_audio);
 			});
-
+			
 			// Start updater
-			CMPUpdate();
+			Start();
 		}
 	});
 	
@@ -190,6 +367,8 @@
 				{
 					main_box.find('.top_audio_player_cmp_author').html(CMP[0][4]); // author
 					main_box.find('.top_audio_player_cmp_name').html(CMP[0][3]); // music name
+					var download_str = 'Скачать ' + main_box.find('.top_audio_player_cmp_author').text() + ' - ' + main_box.find('.top_audio_player_cmp_name').text();
+					main_box.find('.top_audio_player_download').attr('title', download_str);
 					//
 					main_box.find('.top_audio_player_cmp_author').css('opacity', '1');
 					main_box.find('.top_audio_player_cmp_name').css('opacity', '1');
@@ -209,7 +388,6 @@
 				}, 1000);
 			}
 		}
-		setTimeout(CMPUpdate, 200);
 	}
 	
 	function download_file(url, name, type, callback) 
