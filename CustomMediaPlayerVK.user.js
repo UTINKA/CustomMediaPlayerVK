975879c66fa4255ac041329e02b459fd2f01c3f6
UTINKA <artem900079@gmail.com>
2020-05-13 00:26:56 +0300

    Bugs Fixed & Removed Downloading Audios

--- CustomMediaPlayerVK.user.js
+++ CustomMediaPlayerVK.user.js
@@ -2,19 +2,17 @@
 // @date			23.04.2019
 // @name			CustomMediaPlayerVK
 // @namespace		https://github.com/UTINKA/CustomMediaPlayerVK/
-// @version			0.4
+// @version			0.6
 // @description		Изменённый плеер в верхнем меню для ВК
 // @author			UTINKA
 // @include			https://vk.com/*
 
 // @icon
 // @homepage		https://greasyfork.org/ru/scripts/376022-custommediaplayervk
-// @downloadURL		https://greasyfork.org/scripts/376022-custommediaplayervk/code/CustomMediaPlayerVK.user.js
 // @supportURL		
 
 // @grant			none
 // @require			http://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js
-// @require			https://cdnjs.cloudflare.com/ajax/libs/downloadjs/1.4.7/download.min.js
 
 // @run-at			document-end
 // ==/UserScript==
@@ -65,22 +63,22 @@
 			if(cmpvk[0] == 0)
 			{
 				CMPVKsetState(false);
-				$('#settings_cmpvk_state').prop("checked", false);
+				if(location.href.search(/settings/) > 1 && location.href.search(/act/) == -1) $('#settings_cmpvk_state').prop("checked", false);
 			}
 			else if(cmpvk[0] == 1)
 			{
 				CMPVKsetState(true);
-				$('#settings_cmpvk_state').prop("checked", true);
+				if(location.href.search(/settings/) > 1 && location.href.search(/act/) == -1) $('#settings_cmpvk_state').prop("checked", true);
 			}
 			if(cmpvk[1] == 0)
 			{
 				$('.top_audio_player').css('display','block');
-				$('#settings_cmpvk_player').prop("checked", false);
+				if(location.href.search(/settings/) > 1 && location.href.search(/act/) == -1) $('#settings_cmpvk_player').prop("checked", false);
 			}
 			else if(cmpvk[1] == 1)
 			{
 				$('.top_audio_player').css('display','none');
-				$('#settings_cmpvk_player').prop("checked", true);
+				if(location.href.search(/settings/) > 1 && location.href.search(/act/) == -1) $('#settings_cmpvk_player').prop("checked", true);
 			}
 			//
 			var sbox = $('.settings_panel');
@@ -249,24 +247,6 @@
 		background-size: cover;\
 		transition: 0.5s;\
 	}\
-	.top_audio_player_img div{\
-		opacity: 0;\
-		width: 100%;\
-		height: 100%;\
-		background: rgba(0, 0, 0, 0.3);\
-		transition: 0.6s;\
-	}\
-	.top_audio_player_img:hover div{\
-		opacity: 1;\
-	}\
-	.top_audio_player_img div .top_audio_player_download{\
-		display: block;\
-		margin: 0;\
-		width: 24px;\
-		height: 24px;\
-		padding: 9px;\
-		color: #fff;\
-	}\
 	.top_audio_player_cmp_author{\
 		opacity: 0;\
 		position: absolute;\
@@ -278,7 +258,7 @@
 		white-space: nowrap;\
 		text-overflow: ellipsis;\
 		cursor: pointer;\
-		transition: 0.1s;\
+		transition: 0.2s;\
 	}\
 	.top_audio_player_cmp_name{\
 		opacity: 0;\
@@ -291,7 +271,7 @@
 		white-space: nowrap;\
 		text-overflow: ellipsis;\
 		cursor: pointer;\
-		transition: 0.1s;\
+		transition: 0.2s;\
 	}\
 	.top_audio_player_download_state{\
 		opacity: 1;\
@@ -318,21 +298,10 @@
 			');
             main_box.append('\
 			<div class="top_audio_player_img">\
-				<div>\
-					<a class="top_audio_player_btn top_audio_player_download" title="Скачать" href="">\
-						<i class="material-icons">play_for_work</i>\
-					</a>\
-				</div>\
 			</div>\
 			<div class="top_audio_player_download_state">\
 			</div>\
 			' + CMP_CSS);
-
-			main_box.find('.top_audio_player_img').find('div a').click(function(e)
-			{
-				e.preventDefault();
-				download_file(main_box.find('.top_audio_player_img').find('div a')[0].href, CMP[0][4] + ' - ' + CMP[0][3], "audio/mp3", window.download_audio);
-			});
 			
 			// Start updater
 			Start();
@@ -347,13 +316,19 @@
 		CMP[0] = getAudioPlayer()._currentAudio;
 		if(CMP[0] != false)
 		{
-			main_box.find('.top_audio_player_img').find('div a')[0].href = getAudioPlayer()._impl._currentAudioEl.src;
+			//main_box.find('.top_audio_player_img').find('div a')[0].href = getAudioPlayer()._impl._currentAudioEl.src;
 			//
 			var check_track = CMP[0][4] + '_' + CMP[0][3];
 			if(CMP[2] == '' || CMP[2] != check_track)
 			{
-				main_box.find('.top_audio_player_cmp_author').css('opacity', '0');
-				main_box.find('.top_audio_player_cmp_name').css('opacity', '0');
+				main_box.find('.top_audio_player_cmp_author').css({
+					'opacity': '0',
+					'transform': 'translateY(-35px)'
+				});
+				main_box.find('.top_audio_player_cmp_name').css({
+					'opacity': '0',
+					'transform': 'translateY(-25px)'
+				});
 				//
 				if(CMP[0][14].split(',')[1] != undefined)
 				{
@@ -365,17 +340,36 @@
 				clearTimeout(CMP[3]);
 				CMP[3] = setTimeout(function()
 				{
-					main_box.find('.top_audio_player_cmp_author').html(CMP[0][4]); // author
-					main_box.find('.top_audio_player_cmp_name').html(CMP[0][3]); // music name
-					var download_str = 'Скачать ' + main_box.find('.top_audio_player_cmp_author').text() + ' - ' + main_box.find('.top_audio_player_cmp_name').text();
-					main_box.find('.top_audio_player_download').attr('title', download_str);
+					main_box.find('.top_audio_player_cmp_author').css({
+						'opacity': '0',
+						'transform': 'translateY(35px)'
+					});
+					main_box.find('.top_audio_player_cmp_name').css({
+						'opacity': '0',
+						'transform': 'translateY(25px)'
+					});
 					//
-					main_box.find('.top_audio_player_cmp_author').css('opacity', '1');
-					main_box.find('.top_audio_player_cmp_name').css('opacity', '1');
+					clearTimeout(CMP[3]);
+					CMP[3] = setTimeout(function()
+					{
+						main_box.find('.top_audio_player_cmp_author').html(CMP[0][4]); // author
+						main_box.find('.top_audio_player_cmp_name').html(CMP[0][3]); // music name
+						/*var download_str = 'Скачать ' + main_box.find('.top_audio_player_cmp_author').text() + ' - ' + main_box.find('.top_audio_player_cmp_name').text();
+						main_box.find('.top_audio_player_download').attr('title', download_str);*/
+						//
+						main_box.find('.top_audio_player_cmp_author').css({
+							'opacity': '1',
+							'transform': 'translateY(0px)'
+						});
+						main_box.find('.top_audio_player_cmp_name').css({
+							'opacity': '1',
+							'transform': 'translateY(0px)'
+						});
+					}, 100);
 				}, 100);
 			}
 			//
-			if(Math.round(CMP[1], 2) == 100)
+			/*if(Math.round(CMP[1], 2) == 100)
 			{
 				CMP[1] = 0;
 				setTimeout(function()
@@ -386,11 +380,11 @@
 						$('.top_audio_player_download_state').css('width', '0%');
 					}, 500);
 				}, 1000);
-			}
+			}*/
 		}
 	}
 	
-	function download_file(url, name, type, callback) 
+	/*function download_file(url, name, type, callback) 
 	{
 		$('.top_audio_player_download_state').css('opacity', '1');
 		var xhr = new XMLHttpRequest();
@@ -406,5 +400,5 @@
 			$('.top_audio_player_download_state').css('width', Math.round(CMP[1], 2) + '%');
 		};
 		xhr.send();
-	}
+	}*/
 })();
